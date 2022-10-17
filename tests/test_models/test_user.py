1b9aca39604361ffae7bc6e16188ed2ad353b1d7
raycast5 <ray.j.colon@gmail.com>
2022-10-17 05:01:05 -0400

    Added User Tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,7 +11,12 @@ class TestUser(unittest.TestCase):
     """Tests for User Class"""
 
     def test_inst_type(self):
+        u1 = User()
         self.assertEqual(User, type(User()))
+        self.assertIsInstance(u1.email, str)
+        self.assertIsInstance(u1.password, str)
+        self.assertIsInstance(u1.first_name, str)
+        self.assertIsInstance(u1.last_name, str)
 
     def test_storage(self):
         self.assertIn(User(), models.storage.all().values())
