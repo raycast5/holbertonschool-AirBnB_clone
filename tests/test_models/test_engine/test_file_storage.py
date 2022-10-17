94898ae80c50d3a74c25a4230a6461c01f3fb29f
raycast5 <ray.j.colon@gmail.com>
2022-10-17 11:59:25 -0400

    Updated tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 
 import unittest
 
+import models
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
@@ -15,17 +16,20 @@ class TestFileStorage(unittest.TestCase):
         cls.b1 = FileStorage()
         cls.b2 = BaseModel()
 
+    def test_new(self):
+        """Test New"""
+        models.storage.new(self.b2)
+        fullname = "BaseModel." + self.b2.id
+        self.assertIn(fullname, models.storage.all().keys())
+        self.assertIn(self.b2, models.storage.all().values())
+
     def test_save(self):
         """Test Save"""
-        self.b2.save()
+        models.storage.new(self.b2)
+        models.storage.save()
         fullname = "BaseModel." + self.b2.id
         with open("file.json", "r") as f:
             self.assertIn(fullname, f.read())
-
-    def test_save_arg(self):
-        b = BaseModel()
-        with self.assertRaises(TypeError):
-            b.save(None)
 
     def test_all(self):
         """Test if all returns dict"""
@@ -34,10 +38,8 @@ class TestFileStorage(unittest.TestCase):
     def test_no_args(self):
         self.assertEqual(BaseModel, type(BaseModel()))
 
-
     def test_attr(self):
         """Test file storage attributes"""
         self.assertIsInstance(self.b0.get_file_path(), str)
         self.assertEqual(self.b0.get_file_path(), "file.json")
         self.assertIsInstance(self.b0.get_objects(), dict)
-
