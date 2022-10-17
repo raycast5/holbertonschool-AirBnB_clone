a5ecd432fda352ace9e859cb6fdc9ba08b74a041
raycast5 <ray.j.colon@gmail.com>
2022-10-17 12:14:19 -0400

    Edited test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -46,3 +46,30 @@ class TestFileStorage(unittest.TestCase):
         self.assertIsInstance(self.b0.get_file_path(), str)
         self.assertEqual(self.b0.get_file_path(), "file.json")
         self.assertIsInstance(self.b0.get_objects(), dict)
+
+class TestBaseModel(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.b1 = BaseModel()
+        cls.b2 = BaseModel()
+
+    def test_init(self):
+        """Test initialization"""
+        original_date = self.b1.updated_at
+        self.b1.save()
+        self.assertNotEqual(original_date, self.b1.updated_at)
+        self.assertNotEqual(self.b1.created_at, self.b1.updated_at)
+
+    def test_to_dict(self):
+        """Test that it saves a dict"""
+        self.assertIsInstance(self.b1.to_dict(), dict)
+
+    def test_id(self):
+        """Test id"""
+        self.assertNotEqual(self.b1.id, self.b2.id)
+        self.assertIsInstance(self.b1.id, str)
+
+    def test_str(self):
+        """Test if __str__ returns str"""
+        self.assertIsInstance(self.b1.__str__(), str)
